48ebe0284d53ce81819b0b02bd5e6a3f4d284be4
Tawnee <allprt89@gmail.com>
2021-05-11 09:49:29 -0600

    add createPost and createComment mutations

--- src/index.js
+++ src/index.js
@@ -82,6 +82,8 @@ const typeDefs = `
 
   type Mutation {
     createUser(name: String!, email: String!, age: Int): User!
+    createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+    createComment(text: String!, author: ID!, post: ID!): Comment!
   }
 
   type User {
@@ -164,6 +166,45 @@ const resolvers = {
 			users.push(user)
 
 			return user
+		},
+		createPost: (parent, args, ctx, info) => {
+			const userExists = users.some((user) => user.id === args.author)
+
+			if (!userExists) {
+				throw new Error('User not found.')
+			}
+
+			const post = {
+				id: uuid(),
+				title: args.title,
+				body: args.body,
+				published: args.published,
+				author: args.author,
+			}
+
+			posts.push(post)
+
+			return post
+		},
+		createComment: (parent, args, ctx, info) => {
+			const userExists = users.some((user) => user.id === args.author)
+			const postPublished = posts.some(
+				(post) => post.id === args.post && post.published
+			)
+
+			if (!userExists || !postPublished) {
+				throw new Error('Unable to find user or post')
+			}
+
+			const comment = {
+				id: uuid(),
+				text: args.text,
+				post: args.post,
+				author: args.author,
+			}
+
+			comments.push(comment)
+			return comment
 		},
 	},
 	Post: {
