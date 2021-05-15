57163e2b5a7eead44241160631a8c21c4691e3b2
Tawnee <allprt89@gmail.com>
2021-05-14 20:14:20 -0600

    add comment subscriptions

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -169,10 +169,15 @@ export const Mutation = {
 		}
 
 		db.comments.push(comment)
-		pubsub.publish(`comment ${args.data.post}`, { comment })
+		pubsub.publish(`comment ${args.data.post}`, {
+			comment: {
+				mutation: 'CREATED',
+				data: comment,
+			},
+		})
 		return comment
 	},
-	deleteComment: (parent, args, { db }, info) => {
+	deleteComment: (parent, args, { db, pubsub }, info) => {
 		const commentIndex = db.comments.findIndex(
 			(comment) => comment.id === args.id
 		)
@@ -182,10 +187,17 @@ export const Mutation = {
 		}
 
 		db.comments = db.comments.filter((comment) => comment.post !== args.id)
-		const deletedComments = db.comments.splice(commentIndex, 1)
-		return deletedComments[0]
+		const [deletedComment] = db.comments.splice(commentIndex, 1)
+
+		pubsub.publish(`comment ${deletedComment.post}`, {
+			comment: {
+				mutation: 'DELETED',
+				data: deletedComment,
+			},
+		})
+		return deletedComment
 	},
-	updateComment: (parent, args, { db }, info) => {
+	updateComment: (parent, args, { db, pubsub }, info) => {
 		const comment = db.comments.find((comment) => comment.id === args.id)
 
 		if (!comment) {
@@ -195,6 +207,13 @@ export const Mutation = {
 		if (typeof args.data.text === 'string') {
 			comment.text = args.data.text
 		}
+
+		pubsub.publish(`comment ${comment.post}`, {
+			comment: {
+				mutation: 'UPDATED',
+				data: comment,
+			},
+		})
 
 		return comment
 	},
