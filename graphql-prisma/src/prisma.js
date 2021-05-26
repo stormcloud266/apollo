3630d34e3cee4dded1ba028d09a6eb44c60ef509
Tawnee <allprt89@gmail.com>
2021-05-26 10:51:25 -0600

    add create and update post functions

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -5,19 +5,66 @@ const prisma = new Prisma({
 	endpoint: 'http://localhost:4466',
 })
 
-prisma.query
-	.users(null, '{ id name email posts { title id } }')
-	.then((data) => {
-		console.log(JSON.stringify(data, undefined, 2))
-	})
-	.catch((err) => {
-		console.log(err)
-	})
-prisma.query
-	.comments(null, '{ id text author { name id } }')
-	.then((data) => {
-		console.log(JSON.stringify(data, undefined, 2))
-	})
-	.catch((err) => {
-		console.log(err)
-	})
+const createPostForUser = async (authorId, data) => {
+	const post = await prisma.mutation.createPost(
+		{
+			data: {
+				...data,
+				author: {
+					connect: {
+						id: authorId,
+					},
+				},
+			},
+		},
+		'{ id }'
+	)
+
+	const user = await prisma.query.user(
+		{
+			where: {
+				id: authorId,
+			},
+		},
+		'{ id name email posts { id title published } }'
+	)
+
+	return user
+}
+
+const updatePostForUser = async (postId, data) => {
+	const post = await prisma.mutation.updatePost(
+		{
+			where: {
+				id: postId,
+			},
+			data,
+		},
+		'{ author { id } }'
+	)
+
+	const user = await prisma.query.user(
+		{
+			where: {
+				id: post.author.id,
+			},
+		},
+		'{ id name email posts { id title published } }'
+	)
+
+	return user
+}
+
+// createPostForUser('ckouv8hzl00180801p5xscpso', {
+// 	title: 'good books',
+// 	body: 'war of art',
+// 	published: true,
+// }).then((user) => {
+// 	console.log(JSON.stringify(user, undefined, 2))
+// })
+
+// updatePostForUser('ckp5ohci6000r0901hrzid636', {
+// 	title: 'walt whitmans poetry',
+// }).then((user) => {
+// 	console.log(JSON.stringify(user, undefined, 2))
+// })
