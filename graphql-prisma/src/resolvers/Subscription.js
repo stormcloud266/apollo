4833ece870d2dc3122785808a785af49b5bd78eb
Tawnee <allprt89@gmail.com>
2021-06-03 12:00:33 -0600

    connect subscriptions

--- graphql-prisma/src/resolvers/Subscription.js
+++ graphql-prisma/src/resolvers/Subscription.js
@@ -1,17 +1,32 @@
 export const Subscription = {
 	comment: {
-		subscribe: (parent, { postId }, { pubsub, db }, info) => {
-			const post = db.posts.find((post) => post.id === postId && post.published)
-
-			if (!post) {
-				throw new Error('Post not found')
-			}
-
-			return pubsub.asyncIterator(`comment ${postId}`)
+		subscribe: (parent, { postId }, { prisma }, info) => {
+			return prisma.subscription.comment(
+				{
+					where: {
+						node: {
+							post: {
+								id: postId,
+							},
+						},
+					},
+				},
+				info
+			)
 		},
 	},
 	post: {
-		subscribe: (parent, args, { pubsub, db }, info) =>
-			pubsub.asyncIterator(`post`),
+		subscribe: (parent, args, { prisma }, info) => {
+			return prisma.subscription.post(
+				{
+					where: {
+						node: {
+							published: true,
+						},
+					},
+				},
+				info
+			)
+		},
 	},
 }
