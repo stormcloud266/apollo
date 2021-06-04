66dd3c44e5cd010a3ac93031355fed7044d492ef
Tawnee <allprt89@gmail.com>
2021-06-04 12:08:35 -0600

    remove files from git

--- graphql-prisma/src/prisma.js
+++
@@ -1,8 +0,0 @@
-import { Prisma } from 'prisma-binding'
-
-const prisma = new Prisma({
-	typeDefs: 'src/generated/prisma.graphql',
-	endpoint: 'http://localhost:4466',
-})
-
-export { prisma as default }
